533bcf18fa80e5f2762e15e687a5eafbbf46f2bb
Ayush Singh <ayushsinghbharti123@gmail.com>
2024-07-15 23:40:24 +0530

    Basic changes

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -3,26 +3,34 @@ import { FontAwesome, Feather } from "@expo/vector-icons";
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: "black", tabBarShowLabel: false }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          headerTitle: "For you",
-          tabBarIcon: ({color}) => <FontAwesome name="home" size={30} color={color} />,
-        }}
-      />
+    <Tabs
+      screenOptions={{ tabBarActiveTintColor: "black", tabBarShowLabel: false }}
+    >
       <Tabs.Screen
         name="new"
         options={{
           headerTitle: "Add new post",
-          tabBarIcon: ({color}) => <Feather name="plus-square" size={30} color={color} />,
+          tabBarIcon: ({ color }) => (
+            <Feather name="plus-square" size={30} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="feed"
+        options={{
+          headerTitle: "For you",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="home" size={30} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           headerTitle: "Profile Page",
-          tabBarIcon: ({color}) => <FontAwesome name="user" size={30} color={color} />,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="user" size={30} color={color} />
+          ),
         }}
       />
     </Tabs>
--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -1,14 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Pressable,
+} from "react-native";
+import React, { useState } from "react";
 
 const newPost = () => {
+  const [caption, setCaption] = useState("");
   return (
-    <View>
-      <Text>newPost</Text>
+    <View className="p-3 items-center flex-1">
+      {/* IMage Pciker */}
+      <Image
+        source={{
+          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
+        }}
+        className="w-60 aspect-[3/4] bg-gray-700 shadow-2xl rounded-xl"
+      />
+      <Text
+        className="text-blue-500 font-semibold m-5 text-l"
+        onPress={() => {}}
+      >
+        Change
+      </Text>
+
+      {/* Text Input */}
+      <TextInput
+        value={caption}
+        onChangeText={(text) => setCaption(text)}
+        placeholder="Enter name of your post"
+        className="w-full p-3"
+      />
+
+      {/* Button */}
+      <View className="w-full mt-48">
+        <Pressable className="bg-blue-500 w-full p-3 items-center rounded-lg mt-auto">
+          <Text className="text-white font-semibold">Share</Text>
+        </Pressable>
+      </View>
+
     </View>
-  )
-}
+  );
+};
 
-export default newPost
-
-const styles = StyleSheet.create({})
+export default newPost;
