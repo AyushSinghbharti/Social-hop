b5499f32a0bd959a18c0025a5b763eda7f127b8e
Ayush Singh <ayushsinghbharti123@gmail.com>
2024-07-28 01:41:44 +0530

    Fixed advanced image error (Oh god that shit....)

--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -20,30 +20,27 @@ export default function PostListItem({ post }: { post: Interface }) {
   const { width } = useWindowDimensions();
   const image = cld.image(post.image);
 
-  // image.resize(thumbnail().width(width).height(width));
-  // const avatar = cld.image(post.user.avatar_url);
-  // avatar.resize(
-  //   thumbnail().width(48).height(48).gravity(focusOn(FocusOn.face()))
-  // );
+  image.resize(thumbnail().width(width).height(width));
+  const avatar = cld.image(post.user.avatar_url);
+  avatar.resize(
+    thumbnail().width(48).height(48).gravity(focusOn(FocusOn.face()))
+  );
 
   return (
     <View className="bg-white">
       {/* Header */}
       <View className="p-2 flex-row items-center gap-2">
-        {/* <AdvancedImage
+        <AdvancedImage
           cldImg={avatar}
           className="w-12 aspect-square rounded-full"
-        /> */}
+        />
         <Text className="font-semibold">{post.user.username}</Text>
       </View>
-      {/* Content */}
-      {/* <AdvancedImage cldImg={image} className="w-full aspect-square" /> */}
-
+      
       {/* image */}
-      <Image
-        source={{ uri: post.image_url }}
-        className="w-full aspect-square"
-      />
+      {/* Content */}
+      <AdvancedImage cldImg={image} className="w-full aspect-square" />
+
 
       {/* Icons */}
       <View className="flex-row p-3 items-center gap-3">
